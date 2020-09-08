d50a3c912c9d27333dec8a56ba272fd4f48d6247
sd8956 <duquemoreno12@gmail.com>
2020-09-08 16:29:03 -0500

    [FEAT] Add endpont to upload images

---
+++ Api/src/routes/image.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const imageController = require('../controllers/Image');
+const { authenticate } = require('../middleware/auth');
+
+const imageRoutes = express.Router()
+
+imageRoutes.post('/image', authenticate, imageController.upload);
+
+module.exports = imageRoutes
--- Api/src/routes/router.js
+++ Api/src/routes/router.js
@@ -7,6 +7,7 @@ const notesRoutes = require('./notes');
 const roleRoutes = require('./role');
 const stateRoutes = require('./state');
 const userRoutes = require('./user');
+const imageRoutes = require('./image');
 const { authenticate } = require('../middleware/auth');
 
 module.exports = (app) => {
@@ -44,4 +45,6 @@ module.exports = (app) => {
   app.use(stateRoutes);
   // User Routes
   app.use(userRoutes);
+  // Image routes 
+  app.use(imageRoutes);
 }
