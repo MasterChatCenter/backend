836df4a8dd751a3b590dec0808afa5e88c881d65
wilantury <ventas@antury.net>
2020-09-11 11:52:51 -0500

    [ADD] task test

--- webhook/task.py
+++ webhook/task.py
@@ -1,9 +1,26 @@
 
 # Celery
 from celery import Celery
+from celery.schedules import crontab
 import requests
 
 app = Celery('tweet',broker='amqp://admin:mypass@rabbitmq:5672')
+
+app.conf.beat_schedule = {
+    'each 1 min': {
+        'task':'task.sendMessage',
+        'schedule':crontab(minute='*/1')
+    }
+}
+
+@app.task
+def sendMessage():
+    requests.post("http://websocket:5000/api/message", json = {
+            "sid":"6ca6747e237e4c19926540ed92504fe9",
+            "message":"Hi, I need help with something I bought yesterday",
+            "username":"Morty Smith"
+    })
+    return 6+7
 
 @app.task
 def publish(message):
@@ -15,7 +32,7 @@ def publish(message):
     })
 
     requests.post("http://websocket:5000/api/message", json = {
-        "sid": r.user,
-        "message": r.message,
+            "sid":"6ca6747e237e4c19926540ed92504fe9",
+            "message":"Hi, I need help with something I bought yesterday",
+            "username":"Morty Smith"
     })
-    
